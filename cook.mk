a6fabcd59983b9e4ea4c6d9299a1a252885b968d
Will Maier <189851+whilp@users.noreply.github.com>
2026-02-07 18:16:15 +0000

    build: update bootstrap to 2026-02-07-a051c1d
    Required for cosmic.hash and proc.is_main() fix.

--- cook.mk
+++ cook.mk
@@ -9,7 +9,7 @@ type_gen_outputs := $(patsubst %,lib/types/cosmo/%.d.tl,$(type_modules))
 modules += bootstrap
 bootstrap_cosmic := $(o)/bootstrap/cosmic
 bootstrap_files := $(bootstrap_cosmic)
-bootstrap_url := https://github.com/whilp/cosmic/releases/download/2026-01-25-5b9f6cc/cosmic-lua
+bootstrap_url := https://github.com/whilp/cosmic/releases/download/2026-02-07-a051c1d/cosmic-lua
 
 export PATH := $(o)/bootstrap:$(PATH)
 
