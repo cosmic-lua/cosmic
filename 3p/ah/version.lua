78619bde67d7cdcef051ac03bc3517ab21b91eca
Will Maier <189851+whilp@users.noreply.github.com>
2026-02-15 13:42:32 -0800

    bump ah to 2026-02-15-c23c9f6 (#244)
    adds --sandbox, --unveil, --skill, --must-produce, --timeout, --name.

--- 3p/ah/version.lua
+++ 3p/ah/version.lua
@@ -1,6 +1,6 @@
 return {
   format = "binary",
-  platforms = {["*"] = {sha = "32ea226682a500c1c8da44b5635e7a3370f9467ac3f404a2c0146abad4164d63"}},
+  platforms = {["*"] = {sha = "4c5908db097256d20c06d81ace64dfc9521a66c673fc2a28d1cede38933eaad0"}},
   url = "https://github.com/whilp/ah/releases/download/{version}/ah",
-  version = "2026-02-14-2fa4047"
+  version = "2026-02-15-c23c9f6"
 }
