f1ca1138bab189d21b24fa32be06df52ac4fa694
Will Maier <189851+whilp@users.noreply.github.com>
2026-07-02 21:39:51 -0700

    Bump cosmos to 2026.07.03-dff6be237 (slimmed C-core release) (#416)

--- 3p/cosmos/version.lua
+++ 3p/cosmos/version.lua
@@ -1,7 +1,7 @@
 return {
   format = "zip",
-  platforms = {["*"] = {sha = "98a915c93a454991f2162e0ec9382d782cea8441c1d809e86ef51e348ee7d515"}},
+  platforms = {["*"] = {sha = "4785fe49f3a2d49dcdee3c701bc7772151d68b7338f735c5d6497867c67a8cf8"}},
   strip_components = 0,
   url = "https://github.com/whilp/cosmopolitan/releases/download/{version}/cosmos.zip",
-  version = "2026.04.19-97f550ca9"
+  version = "2026.07.03-dff6be237"
 }
