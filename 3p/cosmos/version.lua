f91e36cd772ebce50a1c873eeffaadcd92bd2351
Will Maier <189851+whilp@users.noreply.github.com>
2026-07-03 08:18:05 -0700

    Bump cosmos to 2026.07.03-08e46d2f7; use unix.O_PATH in landlock (#419)

--- 3p/cosmos/version.lua
+++ 3p/cosmos/version.lua
@@ -1,7 +1,7 @@
 return {
   format = "zip",
-  platforms = {["*"] = {sha = "4785fe49f3a2d49dcdee3c701bc7772151d68b7338f735c5d6497867c67a8cf8"}},
+  platforms = {["*"] = {sha = "64561af6517c3cac2427956d0a01d3fe79c2b29fe9407d91a2cde148d21f6ce6"}},
   strip_components = 0,
   url = "https://github.com/whilp/cosmopolitan/releases/download/{version}/cosmos.zip",
-  version = "2026.07.03-dff6be237"
+  version = "2026.07.03-08e46d2f7"
 }
