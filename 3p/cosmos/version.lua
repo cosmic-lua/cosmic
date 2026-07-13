31a3732eec9e1f9949675d6d01b3c9ea73658b87
Will Maier <189851+whilp@users.noreply.github.com>
2026-07-12 19:53:59 -0700

    cosmos: bump pin to 2026.07.13-e6040de22, regen types, adopt new enums (#637)

--- 3p/cosmos/version.lua
+++ 3p/cosmos/version.lua
@@ -1,7 +1,7 @@
 return {
   format = "zip",
-  platforms = {["*"] = {sha = "c4e9ac143cb15b05a31a42fab90694415ae71b6e4c1b940adcab569dcf96acd5"}},
+  platforms = {["*"] = {sha = "e310863816dff2630f6e90a1b7c4ed51a7b82bd9e7fdb5603f7b9308a2e43dad"}},
   strip_components = 0,
   url = "https://github.com/whilp/cosmopolitan/releases/download/{version}/cosmos.zip",
-  version = "2026.07.09-b208ca89f"
+  version = "2026.07.13-e6040de22"
 }
