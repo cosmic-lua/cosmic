8194e943d31b6f46d5aca055231fb84e64bf17a1
Will Maier <189851+whilp@users.noreply.github.com>
2026-07-19 07:16:35 -0700

    shm: deterministic Memory:unmap; cosmos 2026.07.19-b4958a67e (#493) (#700)

--- 3p/cosmos/version.lua
+++ 3p/cosmos/version.lua
@@ -1,7 +1,7 @@
 return {
   format = "zip",
-  platforms = {["*"] = {sha = "9bf704870e52d8ed919323c14a82a7fed92b62a22111292ccee03521f49dd87c"}},
+  platforms = {["*"] = {sha = "612b20cac515ebcdd7d00f0267a79688402eb1843f8253222cccd235afcfdd2a"}},
   strip_components = 0,
   url = "https://github.com/whilp/cosmopolitan/releases/download/{version}/cosmos.zip",
-  version = "2026.07.19-ed74a7a16"
+  version = "2026.07.19-b4958a67e"
 }
