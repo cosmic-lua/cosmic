614f2c5444f30da25b5f651b93bd6cfce64cc833
Will Maier <189851+whilp@users.noreply.github.com>
2026-09-21 23:00:56 -0700

    ci: canonicalize copied driver contexts

--- test/ci/run-driver.sh
+++ test/ci/run-driver.sh
@@ -5,4 +5,5 @@ set -eu
 : "${COSMIC_DRIVER_PROJECT:?COSMIC_DRIVER_PROJECT is required}"
 case "$COSMIC_DRIVER:$COSMIC_DRIVER_PROJECT" in /*:/*) ;; *) exit 1;; esac
 cd "$COSMIC_DRIVER_PROJECT"
+COSMIC_DRIVER_PROJECT=$(pwd -P)
 exec "$COSMIC_DRIVER" "$COSMIC_DRIVER_PROJECT/cosmic_ci/driver.tl" "$@"
