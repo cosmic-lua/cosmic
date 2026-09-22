15930c2791ef0018513938fe02b49e931baa8e82
Will Maier <189851+whilp@users.noreply.github.com>
2026-09-21 22:51:58 -0700

    ci: run copied driver from its project

---
+++ test/ci/run-driver.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+
+: "${COSMIC_DRIVER:?COSMIC_DRIVER is required}"
+: "${COSMIC_DRIVER_PROJECT:?COSMIC_DRIVER_PROJECT is required}"
+case "$COSMIC_DRIVER:$COSMIC_DRIVER_PROJECT" in /*:/*) ;; *) exit 1;; esac
+cd "$COSMIC_DRIVER_PROJECT"
+exec "$COSMIC_DRIVER" "$COSMIC_DRIVER_PROJECT/cosmic_ci/driver.tl" "$@"
